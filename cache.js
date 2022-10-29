4815cb4f6dc3b7ad791f516fa0682c4cbff0f633
Daniel Kuppitz <dan@tangible.store>
2022-10-29 11:18:33 -0700

    allow cache to be used after expiration if builder call fails

--- cache.js
+++ cache.js
@@ -5,18 +5,22 @@ class Cache {
 
   async get(key, builder, expiresInSeconds) {
     const timestamp = new Date().getTime();
-    console.log(this)
     if (this[key]) {
       if (timestamp < this[key].expires) {
         return this[key].value;
       }
     }
-    const value = await builder();
-    this[key] = {
-      expires: timestamp + expiresInSeconds * 1000,
-      value
+    try {
+      const value = await builder();
+      this[key] = {
+        expires: timestamp + expiresInSeconds * 1000,
+        value
+      }
+      return value;
+    } catch (err) {
+      if (this[key]) return this[key].value;
+      throw err;
     }
-    return value;
   }
 }
 
